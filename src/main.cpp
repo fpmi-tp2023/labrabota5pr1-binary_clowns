1dd12461f3e193efa3a0b4c0c58012dc145e8122
Kapitanov Ivan <kapivano@gmail.com>
2023-04-08 21:29:02 +0300

    added authorization menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
 
 int main(){
-    std::cout<<"hi";
+    char* db = "greenhouse.db";
+    while(true){
+        bool authorized;
+        int req;
+        bool stop = false;
+        while (!stop){
+            std::cout<<"You are not authorized\n1: sign in\n2: sign up\n0: exit\n";
+            std::cin>>req;
+            switch(req){
+                case 1:
+                //signing in
+                stop = true;
+                break;
+                case 2:
+                //signing up 
+                break;
+                case 3:
+                return 0;
+                break;
+                default:
+                std::cout<<"Incorrect number, try again!\n";
+                break;
+            }
+        }
+        
+    }
+    
 }
