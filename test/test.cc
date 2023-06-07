31e22413e61ad3b208c2b79cffcc4a8abed3f312
Kapitanov Ivan <kapivano@gmail.com>
2023-06-07 17:08:19 +0300

    make new tests

--- test/test.cc
+++ test/test.cc
@@ -22,9 +22,11 @@ TEST(CheckPasswordTest, TestNegative){
     EXPECT_FALSE(c.checkPassword("admin", "neAdmin"));
 }
 
-TEST(createUserTest, TestPositive){
+TEST(createUserTest, TestPositiveWithDeleting){
     controller c("greenhouse.db");
     EXPECT_TRUE(c.createUser("UniqueUser", "123456789"));
+    EXPECT_TRUE(c.deleteOperatin("Customer", "Login = 'UniqueUser'"));
+    EXPECT_TRUE(c.updateSQLSequence("Customer", "ID"));
 }
 
 TEST(createUserTest, TestNegative){
@@ -36,3 +38,4 @@ TEST(md5Test, TestPositive){
     controller c("greenhouse.db");
     EXPECT_EQ(c.md5("123456"), "e10adc3949ba59abbe56e057f20f883e");
 }
+
